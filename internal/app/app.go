e57517660e3ed8769782582e1acde1916ae0985f
Vladimir <55173559+Hugrid-1@users.noreply.github.com>
2023-10-29 20:55:56 +0300

    refactoring

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,47 +2,10 @@
 package app
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/gin-gonic/gin"
-
 	"github.com/Hugrid-1/Not-Forget/config"
-	"github.com/Hugrid-1/Not-Forget/pkg/httpserver"
-	"github.com/Hugrid-1/Not-Forget/pkg/logger"
-	"github.com/Hugrid-1/Not-Forget/pkg/postgres"
 )
 
 func Run(cfg *config.Config) {
-	l := logger.New(cfg.Log.Level)
+	// l := logger.New(cfg.Log.Level)
 
-	// Repository
-	pg, err := postgres.New(cfg.PG.URL, postgres.MaxPoolSize(cfg.PG.PoolMax))
-	if err != nil {
-		l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
-	}
-	defer pg.Close()
-
-	// HTTP Server
-	handler := gin.New()
-	httpServer := httpserver.New(handler, httpserver.Port(cfg.HTTP.Port))
-
-	// Waiting signal
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-
-	select {
-	case s := <-interrupt:
-		l.Info("app - Run - signal: " + s.String())
-	case err = <-httpServer.Notify():
-		l.Error(fmt.Errorf("app - Run - httpServer.Notify: %w", err))
-	}
-
-	// Shutdown
-	err = httpServer.Shutdown()
-	if err != nil {
-		l.Error(fmt.Errorf("app - Run - httpServer.Shutdown: %w", err))
-	}
 }
