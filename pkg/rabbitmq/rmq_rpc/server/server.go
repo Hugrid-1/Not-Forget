3ecd703577da0525bc8997d00126ca724f98f195
Vladimir <55173559+Hugrid-1@users.noreply.github.com>
2023-10-29 20:25:44 +0300

    Start refactoring
    First time refactoring template.
    Deleting unsuded files and code.

--- pkg/rabbitmq/rmq_rpc/server/server.go
+++ pkg/rabbitmq/rmq_rpc/server/server.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/streadway/amqp"
 
-	"github.com/evrone/go-clean-template/pkg/logger"
-	rmqrpc "github.com/evrone/go-clean-template/pkg/rabbitmq/rmq_rpc"
+	"github.com/Hugrid-1/Not-Forget/pkg/logger"
+	rmqrpc "github.com/Hugrid-1/Not-Forget/pkg/rabbitmq/rmq_rpc"
 )
 
 const (
